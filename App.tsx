d8ba8b0bd4b8af9f5ac2a78ef520d63773f4d1e3
stellastachoviak <stella_ferreira@estudante.sesisenai.org.br>
2025-06-09 17:16:06 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>brat</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
